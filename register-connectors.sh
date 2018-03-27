fe1f91f35b09fa8314b7f2b437f36132402fcb81
Aleksandr Petruhin <rzrbld@users.noreply.github.com>
2018-03-28 02:53:33 +0300

    Update register-connectors.sh

--- register-connectors.sh
+++ register-connectors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "REGISTER SOURCE" && \
-sleep 20 &&\
+sleep 5 &&\
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/tmp/postinstall/jdbc-debezium-postinstall-master/sink-source-postgres.json 
-sleep 20 && \
+sleep 5 && \
 echo "REGISTER TARGET" && \
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/tmp/postinstall/jdbc-debezium-postinstall-master/sink-target-sqlite.json
 echo "REGISTER DONE"

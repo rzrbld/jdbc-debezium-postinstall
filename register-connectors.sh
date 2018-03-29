2229eee883db110b42dd5bb20444c0afdf319c7e
Aleksandr Petruhin <rzrbld@users.noreply.github.com>
2018-03-29 19:33:10 +0300

    Update register-connectors.sh

--- register-connectors.sh
+++ register-connectors.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-echo "REGISTER SOURCE" && \
-sleep 5 &&\
+echo "REGISTER postgres SOURCE" && \
+echo "  " && \
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/tmp/postinstall/jdbc-debezium-postinstall-master/sink-source-postgres.json 
 sleep 5 && \
-echo "REGISTER TARGET" && \
+echo "  " && \
+echo "REGISTER TARGET sqlite" && \
+echo "  " && \
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/tmp/postinstall/jdbc-debezium-postinstall-master/sink-target-sqlite.json
+echo "  " && \
+# uncomment if you have an oracle db, but first - you need to change db host, user and password at /tmp/postinstall/jdbc-debezium-postinstall-master/sink-target-oracle.json file
+#echo "REGISTER TARGET oracle" && \
+#echo "  " && \
+#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @/tmp/postinstall/jdbc-debezium-postinstall-master/sink-target-oracle.json
+#echo "  " && \
 echo "REGISTER DONE"
+
